1a2356c373542a593ac6585c5ac6888b9230b9f8
Leonardo Leggeri <l.leggeri1@studenti.unibg.it>
2026-04-20 22:53:27 +0200

    nuovo eseguibile

---
+++ Tesi/Brugali/src/app/parser/eseguibili/CambioPriorita.d
@@ -0,0 +1,6 @@
+eseguibili/CambioPriorita: sorgenti/CambioPriorita.c \
+ ../../lib/time_library.h ../../lib/trace_marker.h \
+ ../../lib/activity_library_dyn.h
+../../lib/time_library.h:
+../../lib/trace_marker.h:
+../../lib/activity_library_dyn.h:
