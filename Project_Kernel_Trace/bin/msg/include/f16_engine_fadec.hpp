c520dd8842f7c31c8808d636fed605c0560537f5
leo <l.leggeri1@studenti.unibg.it>
2026-06-10 17:04:30 +0200

    nuovi file

---
+++ Project_Kernel_Trace/bin/msg/include/f16_engine_fadec.hpp
@@ -0,0 +1,27 @@
+#ifndef F16_ENGINE_FADEC_HPP
+#define F16_ENGINE_FADEC_HPP
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <stdbool.h>
+
+typedef struct f16_engine_fadec_parameters {
+  char name[15];
+  int period;
+  int parameter;
+  long int deadline;
+} t_f16_engine_fadec_par;
+
+void *PeriodicTask_ENGINE_FADEC(void *ptr);
+void f16_engine_fadec_load(int parameter);
+
+#ifdef __cplusplus
+}
+#endif
+#endif
---
+++ Project_Kernel_Trace/bin/msg/include/f16_radar_gnd.hpp
@@ -0,0 +1,27 @@
+#ifndef F16_RADAR_GND_HPP
+#define F16_RADAR_GND_HPP
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <stdbool.h>
+
+typedef struct f16_radar_gnd_parameters {
+  char name[15];
+  int period;
+  int parameter;
+  long int deadline;
+} t_f16_radar_gnd_par;
+
+void *PeriodicTask_RADAR_GND(void *ptr);
+void f16_radar_gnd_load(int parameter);
+
+#ifdef __cplusplus
+}
+#endif
+#endif
